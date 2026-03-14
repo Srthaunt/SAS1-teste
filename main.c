df9066d5296f3d9bf6c54323fc3fbe85193b86be
Srthaunt <thaunt91@gmail.com>
2026-03-14 20:43:37 -0300

    Add files via upload
    tes
    t

--- main.c
+++ main.c
@@ -2,67 +2,44 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-
-// Pinos
-#define VERMELHO 23
-
-#define VERDE    19
-
-//VARIÁVEL DE ESTADO 
-
-int estado_atual = 0;
-//0=Vermelho
-//1=Verde
-
-
-void inicializar_pinos() {
-    gpio_reset_pin(VERMELHO);
-    gpio_set_direction(VERMELHO, GPIO_MODE_OUTPUT);
-
-    gpio_reset_pin(VERDE);
-    gpio_set_direction(VERDE, GPIO_MODE_OUTPUT);
-}
-
+// João Moreno
+// definir pinos
+#define RED 23
+#define Green 19
 void app_main() {
-    printf("Semáforo pedestre\n");
-    inicializar_pinos();
-
-    while (true) {
-        
-        switch(estado_atual) {
-            case 0: // VERMELHO
-                printf("Pare!\n");
-                gpio_set_level(VERMELHO, 1);
-                 gpio_set_level(VERDE, 0);
-                vTaskDelay(3000 / portTICK_PERIOD_MS);
-                estado_atual = 1; // Próximo passo: Verde
-                break;
-
-            case 1: // VERDE
-                printf("Siga!\n");
-                gpio_set_level(VERMELHO, 0);
-                gpio_set_level(VERDE, 1);
-                vTaskDelay(3000 / portTICK_PERIOD_MS);
-                estado_atual = 2; // Próximo passo: Amarelo
-                break;
-
-            case 2: // vermelho piscante
-                printf("Atenção!\n");
-                gpio_set_level(VERDE, 0);
-                gpio_set_level(VERMELHO, 1);
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                gpio_set_level(VERMELHO, 0);
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                gpio_set_level(VERMELHO, 1);
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                gpio_set_level(VERMELHO, 0);
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                gpio_set_level(VERMELHO, 1);
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                gpio_set_level(VERMELHO, 0);
-                vTaskDelay(100 / portTICK_PERIOD_MS);
-                 estado_atual = 0;
-                break;
-        }
+// varáeis de estado 
+  gpio_set_direction(RED, GPIO_MODE_OUTPUT);
+  gpio_set_direction(Green, GPIO_MODE_OUTPUT);
+int estadoatual = 0;
+int vezes =  5;
+  switch (estadoatual) {
+  // acionamento do led de sinalização vermelha ou pare
+  case 0:
+  printf("pare\n");
+  gpio_set_direction (RED,1);
+  gpio_set_direction (Green,0);
+  vTaskDelay(5000 / portTICK_PERIOD_MS);
+    estadoatual = 0;
+    break; 
+  // acionamento do led de sinalização verde ou continue
+    case 1:
+    printf("avance\n");
+    gpio_set_direction (RED,0);
+    gpio_set_direction (Green,1);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+      estadoatual = 1;
+    break;
+// acionamento do led de sinalização vermelha em estado de cuidado
+        case 3:
+        printf("cuidado\n");
+        for ( int i = 0; i < vezes; i++){
+        gpio_set_direction (RED,1);
+        gpio_set_direction (Green,0);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_direction (RED,0);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        estadoatual = 1;
+          break;
     }
+  }
 }
